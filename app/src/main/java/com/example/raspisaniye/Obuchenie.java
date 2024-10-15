aae1a077e35ece6a2c5d23437914192bd19f9b0a
Vladi2lav <164768894+Vladi2lav@users.noreply.github.com>
2024-10-15 19:51:54 +0500

    ver1

--- app/src/main/java/com/example/raspisaniye/Obuchenie.java
+++ app/src/main/java/com/example/raspisaniye/Obuchenie.java
@@ -8,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Obuchenie extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.privetstvie);
+        setContentView(R.layout.activity_obuchenie);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        String d = "Hello World!";
     }
 }
--- app/src/main/java/com/example/raspisaniye/Privet.java
+++ app/src/main/java/com/example/raspisaniye/Privet.java
@@ -8,17 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity5 extends AppCompatActivity {
+public class Privet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.second_rs);
+        setContentView(R.layout.activity_privetstvie);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        String d = "Hello World!";
     }
 }
