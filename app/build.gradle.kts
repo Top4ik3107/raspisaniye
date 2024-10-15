bbb81aa7fdd254e226a1048c2e397b6a8bdf46f0
Vladi2lav <164768894+Vladi2lav@users.noreply.github.com>
2024-10-15 19:23:59 +0500

    ver1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,10 @@
+
+
 plugins {
+
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
@@ -32,11 +37,15 @@ android {
 }
 
 dependencies {
+    implementation(libs.firebase.analytics)
 
+    implementation(platform(libs.firebase.bom))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
