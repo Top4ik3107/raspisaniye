712098158d9fdaf01546ab831981e992da93e18a
Vladi2lav <164768894+Vladi2lav@users.noreply.github.com>
2024-10-28 23:19:33 +0500

    ver1

--- app/src/main/java/com/example/raspisaniye/Mainr.java
+++ app/src/main/java/com/example/raspisaniye/Mainr.java
@@ -1,6 +1,8 @@
 package com.example.raspisaniye;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class Mainr extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void GoTosecond(View view) {
+        Intent intent = new Intent(this, Secondr.class);
+        startActivity(intent);
     }
 }
