5135bfcdaf8592579ef9823dba19affd55314e90
Vladi2lav <164768894+Vladi2lav@users.noreply.github.com>
2024-10-29 21:15:20 +0500

    ver1

--- app/src/main/java/com/example/raspisaniye/adminpanelPassword.java
+++ app/src/main/java/com/example/raspisaniye/adminpanelPassword.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class adminpanel extends AppCompatActivity {
+public class adminpanelPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_adminpanel_pass);
+        setContentView(R.layout.activity_adminpanel_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
